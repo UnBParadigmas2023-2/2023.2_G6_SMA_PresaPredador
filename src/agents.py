402840983346449db5f2f51fb1b76a301b13baa4
deboracaires <debora.caires.mor@gmail.com>
2023-11-29 19:10:47 -0300

    requisitos que faltam para terminar

--- src/agents.py
+++ src/agents.py
@@ -37,3 +37,20 @@ class Presa(Agent):
             if other_agent is not None:
                 other_agent.vida += 1
                 self.vida -= 1
+    # Colocar para as presas comerem as plantas
+    # Aumentar a vida de acordo com que comem
+    # Movimento de fugir dos predadores
+    # 
+    
+# class Planta(Agent):
+    # Criar o agente da Planta
+    # Criar contador de plantas - nao pode ter mais que um numero fixo de plantas
+    # Criar uma quantidade fixa de plantas
+
+# class Predador(Agent):
+    # Criar o agente do Predador
+    # Criar o movimento aleatório 
+    # Criar a questão da vida do predador
+    # Criar a parte de comer a presa
+    # Criar movimento de reprodução assexuada dos predadores
+    # Criar o movimento de perseguição
